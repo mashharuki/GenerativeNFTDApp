45cf94ca29c1cfd9e6dfd93dc11cf2d5771b3fac
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-06-05 14:35:01 +0900

    rinkebyにデプロイ完了

--- nft-collectible/scripts/deploy.js
+++ nft-collectible/scripts/deploy.js
@@ -1,20 +1,32 @@
-const main = async () => {
-    const NFTCollectibleFactory = await hre.ethers.getContractFactory("NFTCollectible");
-    const NFTContract = await NFTCollectibleFactory.deploy("http://127.0.0.1:5001/ipfs/bafybeihcyruaeza7uyjd6ugicbcrqumejf6uf353e5etdkhotqffwtguva/#/files/json/0");
+async function main() {
+    // baseURI
+    const baseTokenURI = "ipfs://bafybeihcyruaeza7uyjd6ugicbcrqumejf6uf353e5etdkhotqffwtguva/";
+    const [owner] = await hre.ethers.getSigners();
+    // コントラクトをデプロイする。
+    const contractFactory = await hre.ethers.getContractFactory("NFTCollectible");
+    const contract = await contractFactory.deploy(baseTokenURI);
+    await contract.deployed();
 
-    const nftc = await NFTContract.deployed();
+    // コントラクトアドレスをターミナルに出力
+    console.log("NFTCollectible.address:", contract.address);
 
-    console.log("NFTCollectible.address:", nftc.address);
-};
+    // 無料配布用のNFTをMintする。
+    let txn = await contract.reserveNFTs();
+    await txn.wait();
+    console.log("10 NFTs have been reserved");
 
-const runMain = async () => {
-    try {
-        await main();
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
+    // 0.03 ETH を送信して3つ NFT を mint する
+    txn = await contract.mintNFTs(3, { value: hre.ethers.utils.parseEther("0.03"),});
+    await txn.wait();
+
+    // コントラクト所有者の保有するtokenIdsを取得
+    let tokens = await contract.tokensOfOwner(owner.address);
+    console.log("Owner has tokens: ", tokens);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
         process.exit(1);
-    }
-};
-
-runMain();
+});
