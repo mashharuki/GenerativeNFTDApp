7fc600be78406d8a2a7db996abe93ba59729086e
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-06-05 19:05:29 +0900

    フロントも完成

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,22 @@ import Paper from "@mui/material/Paper";
 import Contract from "./contracts/contracts/NFTCollectible.sol/NFTCollectible.json";
 import Footer from './Components/Footer';
 import { ethers } from "ethers";
+import ClipLoader from "react-spinners/ClipLoader";
+import { css } from "@emotion/react";
+import SquirrelsSvg from "./assets/rinkeby_squirrels.gif";
 
 // コントラクトのアドレスとABIを設定
 const CONTRACT_ADDRESS = "0xfe03B6a6B4B095248F06Ed9528e913995ED58f97";
 const ABI = Contract.abi;
 const MAX_SUPPLY = 30;
+const POLYGONSCAN_LINK =`https://mumbai.polygonscan.com/address/${CONTRACT_ADDRESS}`;
+const OPENSEA_LINK = "https://testnets.opensea.io/account";
+
+// スピナー用の変数
+const override = css`
+  display: block;
+  margin: 0 auto;
+`;
 
 /**
  * StyledPaperコンポーネント
@@ -19,10 +30,9 @@ const MAX_SUPPLY = 30;
 const StyledPaper = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(2),
   maxWidth: 600,
-  height: 300,
+  height: 400,
   backgroundColor: '#fde9e8',
 }));
-
 
 /**
  * Appコンポーネント
@@ -31,6 +41,8 @@ function App() {
   // ステート変数
   const [supply, setSupply] = useState(0);
   const [currentAccount, setCurrentAccount] = useState(null);
+  const [mintingFlg, setMintingFlg] = useState(false);
+  const [networkId, setNetworkId] = useState(null);
 
   /**
    * ウォレットの接続状態を確認するメソッド
@@ -42,18 +54,26 @@ function App() {
       console.log("Make sure you have MetaMask installed!");
       return;
     } else {
-      console.log("Wallet exists! We're ready to go!");
-    }
+       // 接続しているチェーンが Rinkebyであることを確認する。
+       let chainId = await ethereum.request({ method: "eth_chainId" });
+       if (chainId !== "0x13881") {
+        alert("You are not connected to the Rinkeby Test Network!");
+       } else {
+          setNetworkId(chainId);
+          // アカウント情報を要求する
+          const accounts = await ethereum.request({ method: "eth_accounts" });
 
-    // アカウント情報を要求する
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account: ", account);
-      setCurrentAccount(account);
-    } else {
-      console.log("No authorized account found");
+          if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log("Found an authorized account: ", account);
+            setCurrentAccount(account);
+            // 発行数を取得する。
+            let totalSupply = await getTotalSupply();
+            setSupply(totalSupply);
+          } else {
+            console.log("No authorized account found");
+          }
+       }
     }
   };
 
@@ -65,15 +85,52 @@ function App() {
 
     if (!ethereum) {
       alert("Please install MetaMask!");
+    } else {
+      // 接続しているチェーンが Rinkebyであることを確認する。
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+      if (chainId !== "0x13881") {
+       alert("You are not connected to the Rinkeby Test Network!");
+      } else {
+        setNetworkId(chainId);
+        try {
+          const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+          console.log("Found an account! Address: ", accounts[0]);
+          // アカウント情報をステート変数にセットする。
+          setCurrentAccount(accounts[0]);
+          // 発行数を取得する。
+          let totalSupply = await getTotalSupply();
+          setSupply(totalSupply);
+        } catch (err) {
+          console.log(err);
+        }
+      }
     }
+  };
 
+  /**
+   * NFTの発行数を取得するメソッド
+   */
+  const getTotalSupply = async() => {
     try {
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-      console.log("Found an account! Address: ", accounts[0]);
-      // アカウント情報をステート変数にセットする。
-      setCurrentAccount(accounts[0]);
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        // コントラクトにアクセスするための準備
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const nftContract = new ethers.Contract(
+          CONTRACT_ADDRESS, 
+          ABI, 
+          signer
+        );
+  
+        // 発行数を取得する。
+        let totalSupply = await nftContract.totalSupply();
+        return totalSupply.toNumber();
+      }
     } catch (err) {
       console.log(err);
+      return 0;
     }
   };
 
@@ -100,10 +157,11 @@ function App() {
           value: ethers.utils.parseEther("0.01"),
         });
   
-        console.log("Mining... please wait");
+        setMintingFlg(true);
         await nftTxn.wait();
   
         console.log(`Mined, see transaction: ${nftTxn.hash}`);
+        setMintingFlg(false);
         alert("Mint Success!!")
       } else {
         console.log("Ethereum object does not exist");
@@ -143,9 +201,21 @@ function App() {
     checkWalletIsConnected();
   }, []);
 
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [networkId]);
+
   return (
     <div className="main-app">
       <h1>Let's Mint Generative NFT !!</h1>
+      <Box sx={{ flexGrow: 1, overflow: "hidden", mt: 4, my: 2}}>
+        <strong>
+          contract address : 
+          <a href={POLYGONSCAN_LINK}>
+            {CONTRACT_ADDRESS}
+          </a>
+        </strong>
+      </Box>
       <Grid
         container
         direction="row"
@@ -153,12 +223,28 @@ function App() {
         alignItems="center"
       >
         <Box sx={{ flexGrow: 1, overflow: "hidden", px: 3, mt: 10}}>
-          <StyledPaper sx={{my: 1, mx: "auto", p: 0, paddingTop: 10, borderRadius: 4}}>  
-            <Box sx={{ flexGrow: 1, overflow: "hidden", mt: 10, my: 5}}>
+          <StyledPaper sx={{my: 1, mx: "auto", p: 0, paddingTop: 2, borderRadius: 4}}>  
+            <Box sx={{ flexGrow: 1, overflow: "hidden", mt: 1, my: 1}}>
               <strong>発行状況：{supply} / {MAX_SUPPLY}</strong>
             </Box>
+            <img src={SquirrelsSvg} alt="Polygon Squirrels" height="40%" /><br/>
             { currentAccount ? mintNftButton() : connectWalletButton()}
+            { mintingFlg ?
+                (
+                  <div>
+                    <ClipLoader color="#99FF99" loading={mintingFlg} css={override} size={35} /><br/>
+                    <div className="spin-color">
+                      Now Minting ...
+                    </div>
+                  </div>
+                ) :<></>
+            }
           </StyledPaper>
+          <button className="opensea-button cta-button">
+              <a href={OPENSEA_LINK}>
+                OpenSeaでNFTを見る
+              </a>
+            </button>
           <Footer/>
         </Box>
       </Grid>
